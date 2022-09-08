b7c41f356844b85fa0dc5b1d43bdb90695479042
Maxim Fadeev <valensiamax@gmail.com>
2022-09-08 16:12:01 +0300

    Declaration User.h and Message.h

--- Message.h
+++ Message.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <iostream>
+
+class Messages
+{
+public:
+	struct TextTime { std::string text_; int hours_; int min_; };
+private:
+	std::string from_;
+	TextTime* pdialog_ = nullptr;
+public:
+	Messages() = default;
+	Messages(const TextTime dialog, const std::string& from);
+	const std::string& getFrom() const;
+	void showDialog() const;
+	void editText();
+	void deleteMessage();
+	~Messages();
+};
--- User.h
+++ User.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <iostream>
+#include "Message.h"
+
+class User
+{
+	std::string name_;
+	std::string password_;
+	std::string login_;
+	std::string* contacts_;
+	Messages* messages_;
+public:
+	User();
+	User(std::string name, std::string password, std::string email);
+	const std::string& getName() const;
+	const std::string& getLogin() const;
+	const std::string& getPassword() const;
+	void showChats() const;
+	const int chooseDialog() const;
+	void showMessages(const int i) const;
+	void setName();
+	void setLogin();
+	void setPassword();
+	void addMessage(const Messages& message);
+	void sendMessage(User& user);
+	Messages editMessage();
+	friend std::ostream& operator<<(std::ostream& os, const User& user);
+	~User();
+};
